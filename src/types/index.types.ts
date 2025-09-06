b11952d8ca70966dbefce971291f59756f5ca7aa
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-06 08:55:52 -0300

    modificamos el archivo de tipos

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -7,3 +7,46 @@ export type MueblesGallery = {
   category: string;
   materials: string[];
 };
+
+export type Colors =
+  | "Blanco"
+  | "Gris claro"
+  | "Gris"
+  | "Plata"
+  | "Gris oscuro"
+  | "Madera natural"
+  | "Roble"
+  | "Hickory"
+  | "Hormigón"
+  | "Nogal"
+  | "Cerámico"
+  | "Negro"
+  | "Color plano"
+  | "Gris Arcilla"
+  | "Gris Macadán"
+  | "Gris Sombra"
+  | string;
+
+export type MaterialsImagesRoute =
+  | "/materiales/Chromix-blanco.webp"
+  | "/materiales/Chromix-plata.webp"
+  | "/materiales/Roble-Davos-natural.webp"
+  | "/materiales/Roble-de-Nebraska-gris.webp"
+  | "/materiales/Roble-de-Nebraska-natural.webp"
+  | "/materiales/Hickory-natural.webp"
+  | "/materiales/Hormigón-Chicago-gris-oscuro.webp"
+  | "/materiales/Nogal-del-Pacífico-natural.webp"
+  | "/materiales/Pietra-Grigia-negro.webp"
+  | "/materiales/Blanco.webp"
+  | "/materiales/Negro.webp"
+  | "/materiales/Gris-arcilla.webp"
+  | "/materiales/Gris-macadán.webp"
+  | "/materiales/Gris-sombra.webp";
+
+export type Materials = {
+  id: number;
+  img: MaterialsImagesRoute;
+  color: Array<Colors>;
+  alt: string;
+  title: string;
+};
