dce9e66aac56ee431fcdb2869d61bf4570dae248
Franco Montechiani <fnmontechiani@gmail.com>
2025-09-06 19:05:12 -0300

    Agregamos un objeto que tiene todos los links y los utilizamos en las vistas de footer y Links para unificarlos

---
+++ src/utils/consts.ts
@@ -0,0 +1,13 @@
+import type { LinksState } from "../types/index.types";
+
+export const links: LinksState = {
+  instagram: "https://www.instagram.com/adm_muebles/",
+  facebook: "https://www.facebook.com/adm.muebles.5",
+  email: "info@adm-muebles.com",
+  phone: "+5493413415656",
+  whatsapp: `https://wa.me/5493413415656`,
+  phoneSpacing: "+54 9 341 341-5656",
+  location: "https://goo.gl/maps/3mX4y7bZz4tX4g9M8",
+  locationString: "Matienzo 3158 - Rosario (Santa Fe)",
+  website: "https://www.adm-muebles.com",
+};
